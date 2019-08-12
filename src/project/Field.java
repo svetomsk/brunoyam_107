b1b4cd45d11f139b2d55b2442aa4bbd08ea1072c
123 <123>
2019-08-12 20:53:23 +0300

    Field is ready

--- src/project/Field.java
+++ src/project/Field.java
@@ -10,7 +10,7 @@ public class Field implements IField {
     public static final int HIDDEN_SHIP = 4;
     public static final int CRASHED_SHIP = 5;
 
-    private int [][] data;
+    private int[][] data;
 
     public Field(String file) {
 
@@ -18,6 +18,36 @@ public class Field implements IField {
 
     public Field(List<Ship> ships) {
         data = new int[10][10];
+        if (!checkShips(ships)) {
+            throw new IllegalArgumentException("Incorrect ships position");
+        }
+    }
+
+    private boolean IncorrectRange(int x, int y) {
+        return x <= 0 || x > 10 || y <= 0 || y > 10;
+    }
+
+    private boolean checkShips(List<Ship> ships) {
+        for (Ship ship : ships) {
+            int x = ship.getX();
+            int y = ship.getY();
+            if (ship.isHorizontal()) {
+                for (int i = 0; i < ship.getSize(); i++) {
+                    if (IncorrectRange(x, y + i) || data[x][y + i] != EMPTY) {
+                        return false;
+                    }
+                    data[x][y + i] = SHIP;
+                }
+            } else {
+                for (int i = 0; i < ship.getSize(); i++) {
+                    if (IncorrectRange(x + i, y) || data[x + i][y] != EMPTY) {
+                        return false;
+                    }
+                    data[x + i][y] = SHIP;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
